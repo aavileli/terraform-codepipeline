2cf7bb657133f91a3598dc046b12bbf0597b4090
Ashley <ashley@bglcorp.com.au>
2017-03-06 16:07:10 +1100

    Added pending jobs

--- awsdata_test.tf
+++ awsdata_test.tf
@@ -9,3 +9,7 @@ data "aws_ecs_cluster" "ecs" {
 output "task_running" {
   value = "${data.aws_ecs_cluster.ecs.running_tasks_count}"
 }
+
+output "task_stopped" {
+  value = "${data.aws_ecs_cluster.ecs.pending_tasks_count}"
+}
